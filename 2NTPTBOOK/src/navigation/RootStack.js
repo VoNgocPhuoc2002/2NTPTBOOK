440d0ec11e249f5f874249238e2b5d330745f604
phuocvnp19667 <vongocphuocit2002@gmail.com>
2023-05-31 16:24:42 +0700

    update cấu trúc thư mục navigation và làm tab navigation

--- 2NTPTBOOK/src/navigation/RootStack.js
+++ 2NTPTBOOK/src/navigation/RootStack.js
@@ -7,7 +7,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 const Stack = createNativeStackNavigator();
 
 const RootStack = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
 
   const handleLogin = () => {
     setIsLoggedIn(false);
---
+++ 2NTPTBOOK/src/screen/cart/CartNoItem.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CartNoItem = () => {
+  return (
+    <View>
+      <Text>CartNoItem</Text>
+    </View>
+  )
+}
+
+export default CartNoItem
+
+const styles = StyleSheet.create({})
