d7ca8f02f0102ae8f44daf4ee3c86b928189ff00
phuocvnp19667 <vongocphuocit2002@gmail.com>
2023-06-26 19:50:16 +0700

    Update projects

--- BookStore/App.js
+++ BookStore/App.js
@@ -2,13 +2,14 @@ import { View, Text } from 'react-native'
 import React from 'react'
 
 import { SafeAreaView } from 'react-native-safe-area-context';
-import RootStack from './src/navigation/RootStack';
-
+import Address1 from './src/screen/addrees/address1';
+// import RootStack from './src/navigation/RootStack';
 
 const App = () => {
   return (
     <SafeAreaView style={{flex:1}}>
-      <RootStack/>
+      {/* <RootStack/> */}
+      <Address1/>
     </SafeAreaView>
    
   )
