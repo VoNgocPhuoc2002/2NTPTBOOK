119a678b735a465e1d2621bcedd7a67e7326f14d
phuocvnp19667 <vongocphuocit2002@gmail.com>
2023-06-27 15:04:26 +0700

    update screen home

--- BookStore/App.js
+++ BookStore/App.js
@@ -2,14 +2,12 @@ import { View, Text } from 'react-native'
 import React from 'react'
 
 import { SafeAreaView } from 'react-native-safe-area-context';
-import Address1 from './src/screen/addrees/address1';
-// import RootStack from './src/navigation/RootStack';
+import RootStack from './src/navigation/RootStack';
 
 const App = () => {
   return (
     <SafeAreaView style={{flex:1}}>
-      {/* <RootStack/> */}
-      <Address1/>
+      <RootStack/>
     </SafeAreaView>
    
   )
--- BookStore/src/screen/home/Styles.js
+++ BookStore/src/screen/home/Styles.js
@@ -10,40 +10,109 @@ const Styles = StyleSheet.create({
         backgroundColor:"blue",
         marginStart:20,
         marginEnd:20,
+        justifyContent: 'space-around',
     },
-    groupHeader:{
+    groupSearch:{
         flexDirection:"row",
-        justifyContent:"space-between",
         alignItems:"center",
-
+        justifyContent:"space-between",
     },
-    groupRight:{
+    inputSearch:{
+        width:"80%",
+        backgroundColor:"white",
+        borderRadius:5,
         flexDirection:"row",
+        alignItems:"center",
+        height:40,
+        paddingStart:10,
     },
-    viewImage:{
-        width:35,
-        height:35,
-        backgroundColor:"red",
-        borderRadius:17.5,
-    },
-
     body:{
-        flex:8,
+        flex:7.5,
     },
-    groupTextHello:{
-        marginStart:20
+
+    // panel
+    groupPanner: {
+        marginTop:15,
+        marginStart: 20,
+        marginEnd: 20,
+      },
+      viewImagePanel: {
+        width: '100%',
+        height:150,
+      },
+      imagePanel: {
+        width: '100%',
+        height:150,
+        borderRadius:15,
+
+      },
+
+    // Sale
+    viewTimePlashSale:{
+        flexDirection:"row",
+        width:"85%",
+        height:50,
+        backgroundColor:"white",
+        justifyContent:"space-around",
+        alignItems:"center",
+        borderRadius:5
+        },
+    viewTextSale:{
+        width:"35%"
+        
     },
-    viewTextAbove:{
-        marginBottom:5,
-    },
-    textAbove:{
-        fontSize:20,
-        color:Constants.COLOR.BLACK,
+    viewGroupTime:{
+        flexDirection:"row",
+        width:"40%",
+        justifyContent:"space-around",
 
     },
-    textBelow:{
-        fontSize:16,
-
+    viewIconNext:{
+        width:"10%",
+        alignItems:"flex-end"
     },
+    viewTextTime:{
+        backgroundColor:"black",
+        height:30,
+       width:40,
+       alignItems: "center",
+       justifyContent: "center",
+       borderRadius:5,
+    },
+    textSale:{
+        color:"red",
+        fontSize:18,
+        fontWeight:"bold",
+    },
+    textTime:{
+        color:"white",
+        fontSize:18,
+        fontWeight:"bold",
+    },
+    groupWorkspaces: {
+        marginStart: 20,
+      },
+      groupItem: {
+        flexDirection: 'row',
+        flex:1,
+      },
+      viewItem: {
+        width: 130,
+        alignItems: 'center',
+        marginEnd:20,
+        backgroundColor:"#F4F5F7",
+        height:170,
+        justifyContent: 'center',
+      },
+    
+      nameItem: {
+        fontSize: 16,
+        color: 'black',
+        fontWeight: 500,
+      },
+      descriptionItem: {
+        marginEnd:10,
+      },
+  
 })
 export default Styles
