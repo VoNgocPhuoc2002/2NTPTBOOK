44994968ac9b1cef4a976a388981c9bfdbaf7968
phuocvnp19667 <vongocphuocit2002@gmail.com>
2023-06-30 18:10:38 +0700

    Delete code bracnh main

--- BookStore/src/hooks/useDimensions.js
+++
@@ -1,9 +0,0 @@
-import { Dimensions } from "react-native";
-
-export default function useDimensions() {
-  const { width, height } = Dimensions.get("window");
-  const screenWidth = width;
-  const screenHeight = height;
-
-  return { screenHeight, screenWidth };
-}
