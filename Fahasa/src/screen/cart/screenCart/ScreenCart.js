77a107bcd35ef211ffeec67deca30a46b26a985a
nhattien2002 <133871393+nhattien2002@users.noreply.github.com>
2023-07-14 12:29:26 +0700

    upload Sreencart

--- Fahasa/src/screen/cart/screenCart/ScreenCart.js
+++ Fahasa/src/screen/cart/screenCart/ScreenCart.js
@@ -1,10 +1,10 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, FlatList, TouchableOpacity, Image, TextInput, ScrollView} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import AxiosIntance from '../../../ultil/AxiosIntance';
 import {getUserId} from '../../../ultil/GetUserId';
 import CartNoItem from '../CartNoItem';
 import styles from './Styles';
-import {CheckBox, Image} from 'react-native-elements';
+import {CheckBox} from 'react-native-elements';
 import { FlashList } from '@shopify/flash-list';
 
 
@@ -12,6 +12,7 @@ const ScreenCart = () => {
   const [data, setData] = useState();
   const [checked, setChecked] = useState(false);
   const [image, setImgae] = useState('');
+  const [quantity, setQuantity] = useState(1);
 
   // Function to fetch cart items
   const fetchShowCart = async () => {
@@ -37,6 +38,73 @@ const ScreenCart = () => {
   }
   // if (data && data.productId.length != null) {
   // If there are cart items, render the items
+
+  const handlePlus = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const handleMinus = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  const DATA = [
+    {
+        id: '1',
+        img: require('../../../assets/sach1.png'),
+        title: 'Combo Sách Giáo Trình Chuẩn HSK 1 - Sách Bài Học',
+        describe1: '1 x Giáo Trình Chuẩn HSK 1 - Sách Bài Tập (2021)',
+        describe2: '1 x Giáo Trình Chuẩn HSK 1 (Tái bản 2022)',
+        price: '270.606 đ',
+    },
+    {
+      id: '2',
+      img: require('../../../assets/sach2.png'),
+      title: 'Combo Sách Muôn Kiếp Nhân Sinh Tập 1 + Không D...',
+      describe1: '1 x Muôn Kiếp Nhân Sinh - Many Times, Many Lives',
+      describe2: '1 x Không Diệt Không Sinh Đừng Sợ Hãi (Tái bản 2022)',
+      price: '198.075 đ',
+    },
+    
+  ]
+
+  const Item = ({ item }) => (
+    <View style={styles.boxItem}>
+      <View style={styles.item}>
+        <View>
+         <CheckBox checked={checked} onPress={() => setChecked(!checked)} />
+        </View>
+        <View>
+          <Image style={styles.imgItem} source={item.img}/>
+        </View>
+        <View>
+          <View style={styles.viewText}>
+            <Text style={styles.textTitle}>{item.title}</Text>
+            <Text style={styles.textDescribe1}>{item.describe1}</Text>
+            <Text style={styles.textDescribe2}>{item.describe2}</Text>
+            <Text style={styles.textPrice}>{item.price}</Text>
+          </View>
+          <View style={styles.viewQuantity}>
+            <TouchableOpacity style={styles.btnTru} onPress={handleMinus}>
+              <Image style={styles.imgTru} source={require('../../../assets/dautru.png')} />
+            </TouchableOpacity>
+            <View style={styles.textQuantity}>
+              <TextInput style={styles.textip} value={quantity.toString()} />
+            </View>
+            <TouchableOpacity style={styles.btnCong}  onPress={handlePlus}>
+              <Image style={styles.imgTru} source={require('../../../assets/daucong.png')} />
+            </TouchableOpacity>
+            <TouchableOpacity  style={styles.btnRemove}>
+              <Image style={styles.imgRemove} source={require('../../../assets/thungrac.png')} />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -49,9 +117,25 @@ const ScreenCart = () => {
             <Text>Chọn tất cả sản phẩm</Text>
           </View>
           <View>
-            <FlashList
-            />
+            <ScrollView>
+            <FlatList style={styles.flatlist}
+                data={DATA}
+                numColumns={1}
+                renderItem={({item}) => <Item item={item}/>}
+                keyExtractor={item => item.id}/>
+            </ScrollView>
             <View>
+              <View style={styles.viewFooter}>
+                <View style={styles.viewThanhTien}>
+                  <Text style={styles.textThanhTien}>Thành tiền</Text>
+                  <Text style={styles.sumPrice}>0 đ</Text>
+                </View>
+                <View >
+                  <TouchableOpacity style={styles.btnThanhToan}>
+                    <Text style={styles.textThanhToan}>Thanh toán</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
               <Image
                 style={{width: 110, height: 120, borderRadius: 10}}
                 source={{uri: image}}
--- Fahasa/src/screen/cart/screenCart/Styles.js
+++ Fahasa/src/screen/cart/screenCart/Styles.js
@@ -25,11 +25,132 @@ const Styles = StyleSheet.create({
         backgroundColor:Constants.COLOR.WHITE,
     },
     groupBody:{
-        backgroundColor:"blue",
+        backgroundColor:"#DDDDDD",
     },
     viewCheckBox:{
         flexDirection:"row",
         alignItems: 'center',
+    },
+    boxItem: {
+        borderRadius: 4,
+        marginTop: 16,
+        // flexDirection: "row",
+        // justifyContent: "space-between",
+        backgroundColor: "white",
+        width: "95%",
+        marginLeft: 10
+    },
+    item:{
+        flexDirection: "row",
+        justifyContent: "space-between",
+        paddingHorizontal: 5,
+        position: "relative",
+    },
+    viewText: {
+        width: 175,
+        position: "relative",
+    },
+    imgItem: {
+        width: 90,
+        height: 90,
+        marginTop: "90%",
+        marginRight: 5
+    },
+    textTitle: {
+        fontSize: 16,
+        color: "black",
+        marginTop: 5,
+    },
+    textDescribe1: {
+        fontSize: 16,
+        color: "black",
+        marginTop: 5
+    },
+    textDescribe2: {
+        fontSize: 16,
+        color: "black",
+    },
+    textPrice: {
+        fontSize: 16,
+        color: "#FF9900",
+        fontWeight: "bold",
+        marginTop: 10,
+    },
+    viewQuantity: {
+        flexDirection: "row",
+        marginBottom: 5,
+        marginTop: 10
+    },
+    btnTru:{
+        backgroundColor: "#DDDDDD",
+        paddingHorizontal: 8,
+        paddingVertical: 4,
+        alignItems:"center",
+        justifyContent:"center",
+    },
+    imgTru: {
+        width: 20,
+        height: 30,
+    },
+    textip: {
+        marginLeft: 10,
+        fontSize: 16,
+    },
+    textQuantity: {
+        backgroundColor: "white",
+        alignItems:"center",
+        justifyContent:"center",
+    },
+    btnCong:{
+        backgroundColor: "#DDDDDD",
+        paddingHorizontal: 8,
+        paddingVertical: 4,
+        alignItems:"center",
+        justifyContent:"center",
+    },
+    imgCong: {
+        width: 20,
+        height: 30,
+    },
+    imgRemove: {
+        width: 50,
+        height: 35,
+        marginLeft: 15,
+        marginTop: 10,
+    },
+    viewFooter: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        backgroundColor: "white",
+        position: "relative",
+        width: "100%",
+    },
+    viewThanhTien: {
+        flexDirection: "column",
+        marginLeft: 10,
+    },
+    textThanhTien: {
+        color: "black",
+        fontSize: 16,
+        marginTop: 10,
+    },
+    sumPrice: {
+        fontSize: 20,
+        color: "#FF9900",
+        fontWeight: "bold",
+    },
+    btnThanhToan: {
+        backgroundColor: "#FF9933",
+        paddingHorizontal: 50,
+        paddingVertical: 15,
+        borderRadius: 120,
+        marginTop: 2,
+        marginRight: 20,
+    },
+    textThanhToan: {
+        color: "white",
+        fontSize: 16,
+        fontWeight: "bold",
     },
 })
 export default Styles
