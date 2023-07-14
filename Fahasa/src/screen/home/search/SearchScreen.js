4a3c8383ee58ec51e6b147c36dde6fbb199b035e
Nguyễn Ngọc Nghĩa <nghonghia53p85956@gmail.com>
2023-07-14 11:49:15 +0700

    update search

--- Fahasa/src/screen/home/search/SearchScreen.js
+++ Fahasa/src/screen/home/search/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import {
   View,
   TextInput,
@@ -18,7 +18,7 @@ const SearchScreen = () => {
   const searchProducts = async () => {
     try {
       const response = await axios.get(
-        `http://192.168.1.179:3000/product/search?keyword=${keyword}`,
+        `http://192.168.102.7:3000/product/search?keyword=${keyword}`,
       );
       const products = response.data;
       setResults(products);
@@ -26,21 +26,110 @@ const SearchScreen = () => {
       console.error(error);
     }
   };
+  const sortByName = () => {
+    const sortedData = [...results];
+
+    sortedData.sort((a, b) => {
+      return a.name.localeCompare(b.name);
+    });
+
+    setResults(sortedData);
+  };
+  const sortByNameZ = () => {
+    const sortedData = [...results];
+  
+    sortedData.sort((a, b) => {
+      return b.name.localeCompare(a.name); // Thay đổi thứ tự so sánh để sắp xếp giảm dần từ Z đến A
+    });
+  
+    setResults(sortedData);
+  };
+  const sortByPriceAscending = () => {
+    const sortedData = [...results];
+
+    sortedData.sort((a, b) => {
+      return a.price - b.price;
+    });
+
+    setResults(sortedData);
+  };
+  const sortByPriceDescending = () => {
+    const sortedData = [...results];
+
+    sortedData.sort((a, b) => {
+      return b.price - a.price;
+    });
+
+    setResults(sortedData);
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <TextInput
-          placeholder="Enter keyword"
-          value={keyword}
-          onChangeText={text => setKeyword(text)}
-        />
-        <Button title="Search" onPress={searchProducts} />
+        <View style={styles.searchBox}>
 
+          <View style={styles.search}>
+            <Image style={{ width: 20, height: 20 }}
+              source={require('../../../assets/IconSearch.png')} />
+            <TextInput
+              placeholder="Sản phẩm cần tìm ..."
+              value={keyword}
+              onChangeText={text => setKeyword(text)}
+            />
+          </View>
+
+          <View style={styles.ButtonBox}>
+            <View style={styles.ButtonBox1}>
+              <View style={styles.Btn_sapxep}>
+
+                
+                <TouchableOpacity style={styles.button} onPress={sortByNameZ}>
+                  <Text>Sắp xếp   </Text>
+                </TouchableOpacity>
+                <Image style={{ width: 20, height: 20 }}
+                  source={require('../../../assets/IconArrange.png')} />
+              </View>
+              <View style={styles.Btn_loc}>
+              <TouchableOpacity style={styles.button} onPress={searchProducts}>
+                  <Text>Lọc  </Text>
+                </TouchableOpacity>
+                <Image style={{ width: 20, height: 20 }}
+                  source={require('../../../assets/IconFilter.png')} />
+              </View>
+            </View>
+            <Button title="Search" onPress={searchProducts} />
+          </View>
+        </View>
         <FlatList
           data={results}
           keyExtractor={item => item._id}
-          renderItem={({item}) => <Text>{item.name}</Text>}
+          renderItem={({ item }) => (
+            <View style={styles.viewItem}>
+              <Image
+                style={{ width: 110, height: 120, borderRadius: 10 }}
+                source={{ uri: item.image }}
+              />
+              <View style={styles.viewItemName}>
+                <Text style={styles.nameItem}>
+                  {item.name.length > 30
+                    ? item.name.substring(0, 35) + '...'
+                    : item.name}
+                </Text>
+              </View>
+              <View style={styles.viewItemNewPrice}>
+                <Text style={styles.newPriceItem}>{item.price}</Text>
+                <View style={styles.viewDiscountItem}>
+                  <Text style={styles.discountItem}>{item.discount}%</Text>
+                </View>
+              </View>
+              <View style={styles.viewItemOldPrice}>
+                <Text>
+                  {item.oldPrice = item.discount === 0 ? item.price : item.price + parseFloat(item.price * (item.discount / 100))}
+                </Text>
+                <View style={styles.line}></View>
+              </View>
+            </View>
+          )}
         />
       </View>
       {/* <View style={styles.body}>
--- Fahasa/src/screen/home/search/Styles.js
+++ Fahasa/src/screen/home/search/Styles.js
@@ -5,52 +5,143 @@ import { Constants } from '../../../Constant'
 
 
 const Styles = StyleSheet.create({
-    container:{
+    container: {
         flex: 1,
     },
     // header
-    header:{
-        flex:2
+    header: {
+        flex: 2,
+       
     },
     // body
-    body:{
-        flex:10,
-        backgroundColor:"red",
+    body: {
+        flex: 10,
+        backgroundColor: "red",
     },
-    groupLeft:{
-        backgroundColor:"blue",
-        width:"30%",
-        alignItems:"center",
-        justifyContent:"center"
+    groupLeft: {
+        backgroundColor: "blue",
+        width: "30%",
+        alignItems: "center",
+        justifyContent: "center"
     },
-    groupRight:{
-        backgroundColor:"green",
-        width:"7`0%",
+    groupRight: {
+        backgroundColor: "green",
+        width: "7`0%",
     },
     viewItem: {
-        backgroundColor:"#F4F5F7",
-        borderRadius:10,
-        flexDirection:"row",
+        backgroundColor: "#F4F5F7",
+        borderRadius: 10,
+        flexDirection: "row",
     },
-    imageItem:{
-        width:80,
-        height:80,
+    imageItem: {
+        width: 80,
+        height: 80,
     },
-    viewItemName:{
-      },
-      nameItem: {
+    viewItemName: {
+    },
+    nameItem: {
         fontSize: 16,
         color: 'black',
-        fontWeight:"bold",
-      },
-      viewItemNewPrice:{
+        fontWeight: "bold",
+    },
+    viewItemNewPrice: {
 
-      },
-      newPriceItem:{
-        fontSize:16,
-        color:Constants.COLOR.DARKRED,
-        fontWeight:"bold",
-      },
-     
+    },
+    newPriceItem: {
+        fontSize: 16,
+        color: Constants.COLOR.DARKRED,
+        fontWeight: "bold",
+    },
+    searchBox: {
+        backgroundColor:Constants.COLOR.DARKRED,    
+    },
+    search: {
+        flexDirection: "row",
+        alignItems: "center",
+        margin:20,
+        backgroundColor:'white',
+        borderRadius:10,
+        paddingLeft:5
+        
+    },
+    ButtonBox1:{
+        flexDirection: "row",
+        alignItems: "center",
+        backgroundColor:'white',
+        justifyContent:"space-evenly",
+        backgroundColor:'white',
+    },
+    Btn_sapxep:{
+        flexDirection: "row",
+        alignItems: "center",
+        margin:20,
+        backgroundColor:'white',
+    },
+    Btn_loc:{
+        flexDirection: "row",
+        alignItems: "center",
+        margin:20,
+        backgroundColor:'white',    
+    },
+   Button:{
+    backgroundColor:'white',
+   },
+   viewItem: {
+    width: "100%",
+    alignItems: 'center',
+    
+    backgroundColor:"#F4F5F7",
+    
+    
+    borderWidth:2,
+    height:240,
+    justifyContent: 'center',
+    
+    
+    
+    
+  },
+  viewItemName:{
+    width:"80%",
+  },
+  nameItem: {
+    fontSize: 14,
+    color: 'black',
+  },
+  viewItemNewPrice:{
+    width:"80%",
+    flexDirection:"row",
+    marginTop:10,
+
+  },
+  newPriceItem:{
+    fontSize:16,
+    color:Constants.COLOR.DARKRED,
+    fontWeight:"bold",
+  },
+  viewDiscountItem:{
+    backgroundColor:Constants.COLOR.DARKRED,
+    width:50,
+    height:25,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginStart:10,
+    borderRadius:5,
+  },
+  discountItem:{
+    fontSize:16,
+    color:Constants.COLOR.WHITE,
+    fontWeight:"bold",
+  },
+  viewItemOldPrice:{
+    width:"80%",
+  },
+  line:{
+    width:"40%",
+    height:2,
+    backgroundColor:Constants.COLOR.BLACK,
+    bottom:10,
+  },
+  
 })
 export default Styles
