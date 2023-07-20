bd791b2c3372669d5e97071973a9d5e2219d560d
phuocvnp19667 <vongocphuocit2002@gmail.com>
2023-07-21 02:20:55 +0700

    create 3 file order

---
+++ Fahasa/src/screen/order/order3/ScreenOrder3.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ScreenOrder3 = () => {
+  return (
+    <View>
+      <Text>ScreenOrder3</Text>
+    </View>
+  )
+}
+
+export default ScreenOrder3
+
+const styles = StyleSheet.create({})
